e2aec858822f574f31dd11d6daab4968334ccfed
Orange Mi <orangemiwj@gmail.com>
2017-03-15 21:14:35 +0800

    update webpack config

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, '../build'),
-    publicPath: '/admin/build'
+    publicPath: '/admin/build/'
   },
   module: {
     rules: [
